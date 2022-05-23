579e7ac517317d0d383a47f4d19d28761ee53dc6
Omar Fayyad <92026691+0xomar@users.noreply.github.com>
2022-05-23 18:07:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import math
 import speedtest
 
-def bytes_to_mb(size_in_bytes):
-    i = int(math.floor(math.log(size_in_bytes,1024)))
-    power = math.pow(1024, i)
-    size = round(size_in_bytes / power, 2)
-    return f"{size} Mbps"
+#a function to convert bits per second to mega bits per second
+#def bytes_to_mb(size_in_bytes):
+#    i = int(math.floor(math.log(size_in_bytes,1024)))
+#    power = math.pow(1024, i)
+#    size = round(size_in_bytes / power, 2)
+#    return f"{size} Mbps"
 
 
-wifi = speedtest.Speedtest()
+wifi = speedtest.Speedtest() # object that can be used for testing download and upload
+print("Loading server list...")
+wifi.get_servers() # get list of servers to test from 
+print("Choosing best server...")
+best_server = wifi.get_best_server() # get best server
+print(f"Found the best sever at: {best_server['host']} located in {best_server['country']}")
 
-print("Getting download speed...")
+print("Performing download speed test...")
 download_speed = wifi.download()
 
 
-print("Getting upload speed...")
+print("Performing upload speed test...")
 upload_speed = wifi.upload()
 
+print("Performing ping test...")
+ping_result = wifi.results.ping
 
-print("\nDownload:", bytes_to_mb(download_speed))
+
+print(f"\nDownload speed:, {download_speed / 1024 / 1024:.2f} Mbps")
 
 
-print("\nUpload:", bytes_to_mb(upload_speed))
+print(f"\nUpload speed:, {upload_speed / 1024 / 1024:.2f} Mbps")
 
-print("\n")
+
+print(f"\nPing result:, {ping_result:.2f} ms")
