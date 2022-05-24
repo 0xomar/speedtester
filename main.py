a5630e96a5d258ad7e1918f1403c0ce2be040c42
Omar Fayyad <92026691+0xomar@users.noreply.github.com>
2022-05-24 12:52:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-#import math
+#!/usr/bin/env python3
 import speedtest
+import schedule
 from contextlib import redirect_stdout
 from time import time, sleep
 from datetime import datetime
-
-#a function to convert bits per second to mega bits per second
-#def bytes_to_mb(size_in_bytes):
-#    i = int(math.floor(math.log(size_in_bytes,1024)))
-#    power = math.pow(1024, i)
-#    size = round(size_in_bytes / power, 2)
-#    return f"{size} Mbps"
 
 
 wifi = speedtest.Speedtest() # object that can be used for testing download and upload
@@ -44,13 +38,20 @@ print(f"\nUpload speed: {upload_speed / 1024 / 1024:.2f} Mbps") # divid result b
 
 print(f"\nPing result: {ping_result:.2f} ms") # have the .2 floor to ONLY two decimals at the .
 
+def script_report():
+    while True:
+        #sleep(60 - time() % 60) #  sleep(60 - time() % 60)
 
-while True:
-    sleep(60 - time() % 60) #  sleep(60 - time() % 60)
-    with open('out.txt', 'a') as f:
-        upload_speed = wifi.upload() # a function to get the upload speed
-        download_speed = wifi.download() # a function to get the download speed
-        dt_now = datetime.now()  # using now function to get date and time now
-        dt_string = dt_now.strftime("%d/%m/%Y %H:%M:%S") # for formatting date and time
-        with redirect_stdout(f):
-	        print(f"\nDate and Time: {dt_string} | Download speed: {download_speed / 1024 / 1024:.2f} Mbps | Upload speed: {upload_speed / 1024 / 1024:.2f} Mbps") # divid result by 1024 twice to get mega bit per second
+        with open('out.txt', 'a') as f:
+            upload_speed = wifi.upload() # a function to get the upload speed
+            download_speed = wifi.download() # a function to get the download speed
+            dt_now = datetime.now()  # using now function to get date and time now
+            dt_string = dt_now.strftime("%d/%m/%Y %H:%M:%S") # for formatting date and time
+            with redirect_stdout(f):
+                print(f"\nDate and Time: {dt_string} | Download speed: {download_speed / 1024 / 1024:.2f} Mbps | Upload speed: {upload_speed / 1024 / 1024:.2f} Mbps") # divid result by 1024 twice to get mega bit per second
+
+schedule.every(2).hours.do(script_report)
+
+while 1:
+    schedule.run_pending()
+    
