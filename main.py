31078f79f31f1fe2fb760dbc1333bd67064e3be3
Omar Fayyad <92026691+0xomar@users.noreply.github.com>
2022-05-29 09:43:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,11 @@ print(f"\nUpload speed: {upload_speed / 1024 / 1024:.2f} Mbps") # divid result b
 
 print(f"\nPing result: {ping_result:.2f} ms") # have the .2 floor to ONLY two decimals at the .
 
+
+with open('out.txt', 'w') as f: #to print on start
+        with redirect_stdout(f):
+            print(f"\nDate and Time: {dt_string} | Download speed: {download_speed / 1024 / 1024:.2f} Mbps | Upload speed: {upload_speed / 1024 / 1024:.2f} Mbps") # divid result by 1024 twice to get mega bit per second
+
 def report_to_db():
     with open('out.txt', 'a') as f:
         upload_speed = wifi.upload() # a function to get the upload speed
