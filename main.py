1e335d1938cd4e9c0806df90a073f8b94855e509
Omar Fayyad <92026691+0xomar@users.noreply.github.com>
2022-06-22 21:00:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,72 @@
 #!/usr/bin/env python3
 import speedtest
 import schedule
-from contextlib import redirect_stdout
-import time
 from datetime import datetime
+import sqlite3
+import os 
+import pandas
+import time
 
 
-wifi = speedtest.Speedtest() # object that can be used for testing download and upload
-print("Loading server list...")
-wifi.get_servers() # get list of servers to test from 
-print("Choosing best server...")
-best_server = wifi.get_best_server() # get best server
-print(f"Found the best sever at: {best_server['host']} located in {best_server['country']}")
 
-print("\nPerforming download speed test...")
-download_speed = wifi.download() # a function to get the download speed
-
-
-print("Performing upload speed test...")
-upload_speed = wifi.upload() # a function to get the upload speed
-
-print("Performing ping test...")
-ping_result = wifi.results.ping # to get the ping results 
+def get_speed():
+    wifi = speedtest.Speedtest() # object that can be used for testing download and upload
+   
+    print("\nLoading Download Speed Using the Best Available Server...\n")
+    wifi.get_servers() # get list of servers to test from 
+    download_speed = wifi.download() # a function to get the download speed
+    speed = int((download_speed / 1024 / 1024)*100)/ 100 # divid result by 1024 twice to get mega bit per second
+    return(speed)
 
 
-# datetime object containing current date and time
-dt_now = datetime.now()  # using now function to get date and time now
-dt_string = dt_now.strftime("%d/%m/%Y %H:%M:%S") # for formatting date and time 
+def db_record(dt_string,speed):
+    db = sqlite3.connect('./speed.db')
+    print('*** Database Connected ***')
+    c = db.cursor()
+    data = [dt_string, speed]
+    sql_stat = 'INSERT INTO download_speed_table VALUES (?,?)'
+    c.execute(sql_stat, data)
+    db.commit()
+    c.close()
+    print('*** Committed Successfully ***')
+    print('*** Closing Database Connection ***\n')
 
-print(f"\nDate and Time now: {dt_string}") # printing Current Date and Time
+def db_connect(speed):
+    # datetime object containing current date and time
+    dt_now = datetime.now()  # using now function to get date and time now
+    dt_string = dt_now.strftime("%Y-%m-%d %H:%M") # for formatting date and time
 
-print(f"\nDownload speed: {download_speed / 1024 / 1024:.2f} Mbps") # divid result by 1024 twice to get mega bit per second
+    if os.path.isfile('./speed.db'):
+        db_record(dt_string,speed)
+        return
+    else:
+        db = sqlite3.connect('./speed.db')
+        print('*** Database Created ***')
+        c = db.cursor()
+        create_table = '''CREATE TABLE download_speed_table (Timestamp text, Speed text)'''
+        c.execute(create_table)
+        print('*** Speed DB Table Created ***')
+        db.commit
+        c.close
+        db_record(dt_string,speed)
+
+def show_records():
+    db = sqlite3.connect('./speed.db')
+    query = 'SELECT * FROM download_speed_table'
+    result = pandas.read_sql_query(query, db)
+    print(result)
 
 
-print(f"\nUpload speed: {upload_speed / 1024 / 1024:.2f} Mbps") # divid result by 1024 twice to get mega bit per second
 
+def main():
+    speed = get_speed()
+    db_connect(speed)
+    show_records()
 
-print(f"\nPing result: {ping_result:.2f} ms") # have the .2 floor to ONLY two decimals at the .
+if __name__ == "__main__":
+    main()
 
-
-with open('out.txt', 'a') as f:
-        with redirect_stdout(f):
-            print(f"\nDate and Time: {dt_string} | Download speed: {download_speed / 1024 / 1024:.2f} Mbps | Upload speed: {upload_speed / 1024 / 1024:.2f} Mbps") # divid result by 1024 twice to get mega bit per second
-
-def report_to_db():
-    with open('out.txt', 'a') as f:
-        upload_speed = wifi.upload() # a function to get the upload speed
-        download_speed = wifi.download() # a function to get the download speed
-        dt_now = datetime.now()  # using now function to get date and time now
-        dt_string = dt_now.strftime("%d/%m/%Y %H:%M:%S") # for formatting date and time
-        with redirect_stdout(f):
-            print(f"\nDate and Time: {dt_string} | Download speed: {download_speed / 1024 / 1024:.2f} Mbps | Upload speed: {upload_speed / 1024 / 1024:.2f} Mbps") # divid result by 1024 twice to get mega bit per second
-schedule.every(2).hours.do(report_to_db)
+schedule.every(2).hours.do(main)
 while True:
     schedule.run_pending()
     time.sleep(1)
